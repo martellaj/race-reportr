fb74eead79703cce129c84774a926acfa13bcc64
Joe Martella <martellaj@live.com>
2016-12-21 22:49:30 -0800

    Initial add of GoalInput component.

---
+++ src/components/GoalInput/GoalInput.jsx
@@ -0,0 +1,12 @@
+import './GoalInput.css';
+import React, { Component } from 'react';
+
+export default class GoalInput extends Component {
+    render() {
+        return (
+            <div>
+                <h3>goal goes here</h3>
+            </div>
+        );
+    }
+}
