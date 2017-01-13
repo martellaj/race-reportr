c7e65f241a6a525ba39c7caae725f94920f4be52
Joe Martella <martellaj@live.com>
2017-01-13 08:50:18 -0800

    Adds placeholder function to log copying of source.

--- src/components/SourceView/SourceView.jsx
+++ src/components/SourceView/SourceView.jsx
@@ -5,17 +5,33 @@ export default class SourceView extends Component {
     constructor() {
         super();
 
+        this.state = {
+            copied: false
+        };
+
         this.onTextAreaFocus = this.onTextAreaFocus.bind(this);
+        this.onCopy = this.onCopy.bind(this);
     }
 
     onTextAreaFocus() {
         this.textarea.select();
     }
 
+    // TODO: Somehow log that a report was copied. Best I can do to count
+    // how many reports have been generated.
+    onCopy() {
+        if (!this.state.copied) {
+            console.log('copied');
+            this.setState({
+                copied: true
+            });
+        }
+    }
+
     render() {
         return (
             <div className="sourceView">
-                <textarea ref={(textarea) => { this.textarea = textarea; }} className="sourceView__textArea" readOnly value={this.props.markdown} onFocus={this.onTextAreaFocus} />
+                <textarea ref={(textarea) => { this.textarea = textarea; }} className="sourceView__textArea" readOnly value={this.props.markdown} onFocus={this.onTextAreaFocus} onCopy={this.onCopy} />
             </div>
         );
     }
